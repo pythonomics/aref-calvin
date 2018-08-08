dd6b0ff68547fdb7cb04a1e012265fd16f72e03d
pythonomics <42207090+pythonomics@users.noreply.github.com>
2018-08-08 15:32:33 +0300

    Test Upload

---
+++ dbtest.py
@@ -0,0 +1,17 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 4444)
+
+db = client.firstdb
+
+users = db.users
+users_data = {
+        'username': 'Ting',
+        'password': 'ting123',
+}
+
+result = users.insert_one(users_data)
+print("One post:{0}".format(result.inserted_id))
+
+print "Done" 
